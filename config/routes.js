10587f498a4e7026ba0ae050bc1862cb63e82123
Stephen MacNeil <smacneil01@gmail.com>
2013-09-18 16:03:10 -0400

    Fixed bug in signup.

--- config/routes.js
+++ config/routes.js
@@ -3,10 +3,22 @@ module.exports = function(app,passport){
 	//home route
 	var home = require('../app/controllers/home');
 	app.get('/', home.index);
-    
+
+    //user routes
     var users = require('../app/controllers/users')
+    app.get('/signup', users.signup)
+    app.post('/users', users.create)
+   
+    //app.get('/login', users.login)
+    //app.get('/logout', users.logout)
 
-
+    //authentication
+    /*app.post('/users/session',
+        passport.authenticate('local', {
+            failureRedirect: '/login',
+            failureFlash: 'Invalid email or password.'
+        }), users.session)
+*/
     app.get('/auth/twitter',
          passport.authenticate('twitter'));
 
@@ -18,13 +30,12 @@ module.exports = function(app,passport){
         }
     );
   
-    app.get('/signup', users.signup)
-    app.get('/register', users.register)
-    app.post('/users', users.create)
 
+
+
+    //Gallery Routes (D3 pages, gallery)
 
     //app.get /assignment#/username;
-
 
     app.get('/twitter', function(req, res){
         res.send('id: ' + req.query.id);
