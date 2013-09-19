457f64047a2594d979fc3dff158d715895e49611
Stephen MacNeil <smacneil01@gmail.com>
2013-09-19 19:44:33 -0400

    Can log in and log out

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose'),
   Article = mongoose.model('Article');
 
 exports.index = function(req, res){
+  user = req.user 
   Article.find(function(err, articles){
     if(err) throw new Error(err);
     res.render('home/index', {
+      
       title: 'Main Page',
       articles: articles
     });
