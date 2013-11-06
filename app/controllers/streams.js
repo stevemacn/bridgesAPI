eab3307c1437814e34755f785d3f3f7fb843e01e
Sean Gallagher <stgallag@gmail.com>
2013-11-06 07:43:59 -0500

    If there was a stream, you should be able to read it

--- app/controllers/streams.js
+++ app/controllers/streams.js
@@ -8,6 +8,17 @@ exports.read = function(req, res) {
         uid: req.user._id,
         domain: req.account_domain
     }, function(err, account) {
-        // STUB
+        if (err)
+            return next(err);
+        var stream = account.streams.filter(function(s) {
+            return s.name == res.stream_name})[0]
+        if (!stream)
+            return next(new Error("Could not find stream " + res.stream_name));
+        if (stream.last_updated < 15*60*1000)
+            // 15 minutes
+            process.nextTick(function() {
+                // TODO: fire off update if the cache is stale  
+            });
+        res.send(stream.content);
     });
 }
