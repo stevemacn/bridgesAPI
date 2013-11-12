40722d859762d133de27ac27edc736744b81ee22
Stephen MacNeil <smacneil01@gmail.com>
2013-11-12 12:03:21 -0500

    adding skeleton for galleries and assignments

--- config/routes.js
+++ config/routes.js
@@ -19,6 +19,17 @@ module.exports = function(app,passport, streamable){
     var streams = require('../app/controllers/streams.js')
     app.get('/streams/:domain/*', streamable, streams.getSource)
 
+    //assignment routes
+    //var assignments = require('../app/controllers/assignments.js')
+    //app.post('/assignments/:assignmentNumber', assignments.upload)
+    //app.get('/assignments/:username/:assignmentNumber', assignments.viewD3)
+    
+    //gallery routes
+    //var gallery = require('../app/controllers/gallery.js')
+    //app.get('/assignments/', gallery.view)
+    //app.get('/assignments/:username', gallery.view)
+
+
     //authentication
     app.post('/users/session',
         passport.authenticate('local', {
@@ -47,11 +58,5 @@ module.exports = function(app,passport, streamable){
                 });
             }
         );
-    //Gallery Routes (D3 pages, gallery)
 
-    //app.get /assignment#/username;
-
-    app.get('/twitter', function(req, res){
-        res.send('id: ' + req.query.id);
-    });
 };
