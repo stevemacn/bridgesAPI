ab1fb1304d1393599e8bc88f0141e5d8ca306ac5
Stephen MacNeil <smacneil01@gmail.com>
2014-01-09 23:52:58 -0500

    made the home display show how user is authenticated

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -2,29 +2,31 @@ var mongoose = require('mongoose')
     , User = mongoose.model('User')
     , Account = mongoose.model('Account')
 
-exports.index = function (req, res) {
-    user = req.user
-    res.render('home/index', {})
-}
-
-//Set up the log in
+//Setup for logging in via twitter
 var login = function (req, res) {
-  if (req.session.returnTo) {
-      res.redirect(req.session.returnTo)
-      delete req.session.returnTo
-      return
+    if (req.session.returnTo) {
+        res.redirect(req.session.returnTo)
+        delete req.session.returnTo
+        return
     }
-  return res.redirect('/home')  
+    return res.redirect('/home')  
 }
 
 exports.authCallback = login
 exports.session = login
 
+
+
+exports.index = function (req, res) {
+    user = req.user
+    res.render('home/index', {})
+}
 exports.login = function (req, res) {
+    msg = req.flash('loginMessage')
     res.render("users/login", {
         title: 'Login',
-        message: req.flash('error')
-
+        //message: req.flash('error')
+        message: msg
    })
 }
 
@@ -37,65 +39,59 @@ exports.logout = function (req, res) {
 exports.display = function (req, res) {
     
     if (!req.user) return res.redirect("login")
-        
+    
     var accounts
         , user = req.user
-     
+
     Account
-        .find({ email : user.email })
+        .findOne({ email : user.email })
         .exec(function (err, accts) {
             if (err) return next(err)
-            if (!accts) return next(new Error('Failed to load User ' + id))
-            accounts=accts        
+            //if (!accts) return next(new Error('Failed to load User ' + id))
+            res.render('users/index', {
+                title: user.username + "'s Dashboard - Bridges",
+                user: user,
+                acct: accts
+            })
         })
-
     
-    res.render('users/index', {
-      title: user.username + "'s Dashboard - Bridges",
-      user: user
-    })
-}
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) { return next(); }
-  res.redirect('/login')
 }
 
 //set up the signup
 exports.signup = function (req, res) {
-  res.render('users/signup', {
-      title: 'Sign up',
-      user: new User()
-  })
+    res.render('users/signup', {
+        title: 'Sign up',
+        user: new User()
+    })
 }
 
 exports.create = function (req, res) {
-  var user = new User(req.body)
-  user.provider = 'local'
-  user.save(function (err) {
-      if (err) {
+    var user = new User(req.body)
+    user.provider = 'local'
+    user.save(function (err) {
+        if (err) {
             return res.render('users/signup', {
                     errors: (err.errors),
                     user: user,
                     title: 'Sign up'
              })
-      }
+        }
   
-      // manually login the user once successfully signed up
-      req.logIn(user, function(err) {
-          if (err) return next(err)
-                  return res.redirect('/')
-      })
-  })
+        // manually login the user once successfully signed up
+        req.logIn(user, function(err) {
+            if (err) return next(err)
+                return res.redirect('/')
+        })
+    })
 }
 
 exports.user = function (req, res, next, id) {
-  User
-    .findOne({ _id : id })
-    .exec(function (err, user) {
-      if (err) return next(err)
-      if (!user) return next(new Error('Failed to load User ' + id))
-      req.profile = user
-      next()
-    })
+    User
+        .findOne({ _id : id })
+        .exec(function (err, user) {
+            if (err) return next(err)
+            if (!user) return next(new Error('Failed to load User ' + id))
+                req.profile = user
+            next()
+        })
 }
