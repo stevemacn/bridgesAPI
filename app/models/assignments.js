bc27e919d916ecf1e23dffda27e925769f2e58a6
smacnie2 <smacneil01@gmail.com>
2014-02-20 16:22:29 -0500

    setup route and update the model

--- app/models/assignments.js
+++ app/models/assignments.js
@@ -6,14 +6,19 @@ var mongoose = require('mongoose')
 
 var Assignment = new Schema ({
 
-    email: { type: String, default: ''},
-    
-    assignmentNumber: {type: String, default: ''},
-    dateCreated: {type: String, default: ''},
+    //For tracking submissions and route creation
+    email:              { type: String, default: ''},
+    assignmentNumber:   {type: String, default: ''},
+    dateCreated:        {type: String, default: ''},
 
-    //Data source content before and after being manipulated
-    originalContent: {type: String, default: ''},
-    updatedContent: {type: String, default: ''}
+    //uploaded data, nodes can be x,y as in scatteplots, links can be any association 
+    nodes: [Schema.Types.Mixed],
+    links: [{ 
+        to:     {type: String, default: ''},
+        from:   {type: String, default: ''},
+        value:  {type: String, default: ''}
+    }]
+    //add a before and after so that student can see changes
 })
 
 mongoose.model('Assignment', Assignment);
