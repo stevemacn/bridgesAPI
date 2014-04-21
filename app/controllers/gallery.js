1513425c2238fd4448e24c669516a1179f45ab57
Stephen MacNeil <smacneil01@gmail.com>
2014-04-20 22:45:47 -0400

    get by username

--- app/controllers/gallery.js
+++ app/controllers/gallery.js
@@ -4,7 +4,19 @@ var mongoose = require('mongoose')
     , Assignment = mongoose.model('Assignment')
 
 exports.view = function (req, res) {
-    
+   
+    var getUsername = function(users, usernames, cb) {
+        if (users.length==0) return cb(usernames)
+        var user = users.pop()
+        User
+            .findOne({"email":user})
+            .exec(function (err, user) {
+                if (err) return null;
+                if (user) usernames.push(user.username)
+                getUsername(users, usernames, cb)
+            })
+    }
+
     Assignment
         .find({
             assignmentID: req.params.assignmentNumber,
@@ -14,12 +26,13 @@ exports.view = function (req, res) {
             if (err) return res.json({"error":err})
             if (!assignmentResult) 
                 return res.json({"error":"could not find assignment"})
-            console.log(assignmentResult.length)
+            
             var users = []
-            for (i=0; i<assignmentResult.length; i++) {
-                console.log(assignmentResult[i].email)
+            for (i=0; i<assignmentResult.length; i++) 
                 users.push(assignmentResult[i].email)
-            }
-            res.send(users) 
+
+            getUsername(users, [], function(usernames) {
+                res.send(usernames) 
+            })
         })
 }
