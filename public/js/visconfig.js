24a2ac81c988ac320d4ed3ade49668b4f72c9729
Stephen MacNeil <smacneil01@gmail.com>
2014-07-17 13:12:46 -0400

    add a generalized way to configure visualizations

--- public/js/visconfig.js
+++ public/js/visconfig.js
@@ -2,4 +2,5 @@ var ele = document.getElementById("vis")
     , width = ele.offsetWidth
      height = ele.offsetHeight
 
-d3.bst(d3, "#vis", width, height)
+bst = d3.bst(d3, "#vis", width, height)
+bst.make(data[0])
