81ac933dfc8192598e703fc6bf2ab00022a47734
Stephen MacNeil <smacneil01@gmail.com>
2014-07-23 20:08:48 -0400

    cleaned up some formatting

--- app/controllers/gallery.js
+++ app/controllers/gallery.js
@@ -3,47 +3,47 @@ var mongoose = require('mongoose'),
     Account = mongoose.model('Account'),
     Assignment = mongoose.model('Assignment')
 
-    exports.view = function(req, res) {
+exports.view = function(req, res) {
 
-        var getUsername = function(users, usernames, cb) {
-            if (users.length == 0) return cb(usernames)
-            var user = users.pop()
-            User
-                .findOne({
-                    "email": user
-                })
-                .exec(function(err, user) {
-                    if (err) return null;
-                    if (user) usernames.push(user.username)
-                    getUsername(users, usernames, cb)
-                })
-        }
-
-        if (!req.params.assignmentNumber) 
-            return next("no assignment number provided") 
-
-        Assignment
-            .find({
-                assignmentID: req.params.assignmentNumber,
-                shared: true
+    var getUsername = function(users, usernames, cb) {
+        if (users.length == 0) return cb(usernames)
+        var user = users.pop()
+        User
+            .findOne({
+                "email": user
             })
-            .exec(function(err, assignmentResult) {
-                if (err) return next(err)
-                    
-                if (!assignmentResult) return next("could not find " +
-                    "assignemnt " + req.params.assignmentNumber)
-
-                var users = []
-                for (i = 0; i < assignmentResult.length; i++)
-                    users.push(assignmentResult[i].email)
-
-                getUsername(users, [], function(usernames) {
-
-                    return res.render('assignments/gallery', {
-                        "user":req.user,
-                        "title": "Assignment gallery",
-                        "usernames": usernames
-                    })
-                })
+            .exec(function(err, user) {
+                if (err) return null;
+                if (user) usernames.push(user.username)
+                getUsername(users, usernames, cb)
             })
     }
+
+    if (!req.params.assignmentNumber) 
+        return next("no assignment number provided") 
+
+    Assignment
+        .find({
+            assignmentID: req.params.assignmentNumber,
+            shared: true
+        })
+        .exec(function(err, assignmentResult) {
+            if (err) return next(err)
+                
+            if (!assignmentResult) return next("could not find " +
+                "assignemnt " + req.params.assignmentNumber)
+
+            var users = []
+            for (i = 0; i < assignmentResult.length; i++)
+                users.push(assignmentResult[i].email)
+
+            getUsername(users, [], function(usernames) {
+
+                return res.render('assignments/gallery', {
+                    "title": "Assignment gallery",
+                    "user":req.user,
+                    "usernames": usernames
+                })
+            })
+        })
+}
