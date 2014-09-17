4b42f619eeeeb01619473ebf65cf2c7dfd9aa269
Stephen MacNeil <smacneil01@gmail.com>
2014-09-17 12:01:10 -0400

    new visualization type

---
+++ public/js/queue.js
@@ -0,0 +1,15 @@
+//This code is heavily based on code from 
+//http://mbostock.github.io/d3/talk/20111018/tree.html 
+
+d3.queue = function(d3, canvasID, w, h, data) {
+
+    var spacing = w / data.length;
+
+    var chart = d3.select(".chart").append("svg")
+        .attr("width", w)
+        .attr("height", h)
+
+    
+
+        
+}
