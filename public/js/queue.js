277c344c7fa8c1d9dc31241532c7f08cb1aa6f32
Stephen MacNeil <smacneil01@gmail.com>
2014-09-17 13:46:02 -0400

    sanitize inputs values

--- public/js/queue.js
+++ public/js/queue.js
@@ -9,35 +9,41 @@ d3.queue = function(d3, canvasID, w, h, data) {
     var chart = d3.select(canvasID).append("svg")
         .attr("width", w)
         .attr("height", h)
+        .style("margin-left", 25)
 
     var nodes = chart.selectAll("g")
         .data(data)
         .enter().append("g")
         .attr("transform", function(d, i) {
-            size = d.size || defaultSize;
-            return "translate(" + 
-                i*spacing+","+(h/2 - size/2)+")";
+            size = parseFloat(d.size || defaultSize);
+            return "translate(" +
+                i * spacing + 10 + "," + (h / 2 - size / 2) + ")";
         })
 
     nodes.append("rect")
         .attr("height", function(d) {
-            return d.size || defaultSize;
+            return parseFloat(d.size || defaultSize);
         })
         .attr("width", function(d) {
-            return d.size || defaultSize;
+            return parseFloat(d.size || defaultSize);
         })
-        .style("fill", function (d) {
-            return d.color || "steelblue"  
+        .style("fill", function(d) {
+            return d.color || "steelblue"
         })
         .style("stroke", "gray")
         .style("stroke-width", 2)
-    
-    nodes    
+
+    nodes
         .append("text")
-            .text(function(d) { return d.name})
-            .attr("x", 0)
-            .attr("y", function(d) {return 15+ (d.size||defaultSize)})
-            .attr("dy", ".35em") 
+        .text(function(d) {
+            return d.name
+        })
+        .attr("x", 0)
+        .attr("y", function(d) {
+            size = parseFloat(d.size||defaultSize)
+            return 15 + size
+        })
+        .attr("dy", ".35em")
 
     //we don't want to process the first node
     data.pop()
@@ -46,12 +52,12 @@ d3.queue = function(d3, canvasID, w, h, data) {
         .data(data)
         .enter().append("line")
         .attr("x1", function(d, i) {
-            return (i) * spacing + d.size || defaultSize;
+            return (i) * spacing + parseFloat(d.size || defaultSize);
         })
         .attr("x2", function(d, i) {
             return (i + 1) * spacing
         })
-        .attr("y1", h / 2) 
+        .attr("y1", h / 2)
         .attr("y2", h / 2)
         .style("stroke", "black")
 }
