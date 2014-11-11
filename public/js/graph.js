a87fb805acd38355e93f24f03864181e79af6c8b
Stephen MacNeil <smacneil01@gmail.com>
2014-11-11 14:26:47 -0500

    fix width height

--- public/js/graph.js
+++ public/js/graph.js
@@ -9,8 +9,8 @@ for (i in links) {
 }
 
 var ele = document.getElementById("vis")
-    , width = ele.offsetWidth+1000
-     height = ele.offsetHeight+1000
+    , width = ele.offsetWidth+5000
+     height = ele.offsetHeight+5000
 
 var force = d3.layout.force()
     .charge([-250])
