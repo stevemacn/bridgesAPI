a8db8ec6cd024c748668011ae9294c2ab4669438
squeetus <burlinsond2@gmail.com>
2015-08-28 15:53:41 -0400

    attempting to test updates

--- app/controllers/gallery_2.js
+++ app/controllers/gallery_2.js
@@ -4,6 +4,8 @@ var mongoose = require('mongoose'),
     Assignment = mongoose.model('Assignment')
 
 exports.view = function(req, res) {
+  return next("Hello");
+  
     var getAssignments = function(assig, assignmentsRes, cb) {
         //console.log(assig, assignmentsRes);
         if (assig.length == 0) return cb(assignmentsRes)
