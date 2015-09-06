ca0a2a835eeecfdb4a1bdcc52345d733695b2b96
squeetus <burlinsond2@gmail.com>
2015-09-05 21:39:09 -0400

    Testing assignments

--- app/controllers/gallery_2.js
+++ app/controllers/gallery_2.js
@@ -7,14 +7,17 @@ exports.view = function(req, res, next) {
 
     var getAssignments = function(assig, assignmentsRes, cb) {
         //console.log(assig, assignmentsRes);
-        if (assig.length == 0) return cb(assignmentsRes)
+        if (assig.length == 0)  {
+          var testString = ":: no assignments found"
+          return cb(assignmentsRes, testString)
+        }
             var assID = assig.pop()
-            // console.log(assID)
+
             Assignment
                 .findOne({
 //                         "assignmentID": assID
                             "assignmentNumber": assID,
-                            "subAssignment": 0
+                            "subAssignment": "00"
                          })
                 .exec(function(err, assID) {
                       if (err) return null;
@@ -38,51 +41,48 @@ exports.view = function(req, res, next) {
 
                 if(!userResult) return next("could not find user")
 
+                Assignment
+                    .find({
+                        //email: req.params.userNameRes,
+                        email: userResult.email,
+                        subAssignment: "00"
+                        //$or: [{assignmentID: /.00$/}, {assignmentID: /.0$/}] //Search for assignments with whole numbers
+                    })
+        //            .limit( 5 )   //Do we want to load every single whole number assignment, or just some? Query might be time intensive.
+                    .sort({
+                        assignmentID: -1
+                    })
+                    .exec(function(err, assignmentResult) {
+                        if (err) return next(err)
 
-//            })
+                        if (!assignmentResult) return next("could not find " +
+                                                             "assignment " + req.params.userNameRes)
 
-        Assignment
-            .find({
-                  //email: req.params.userNameRes,
-                email: userResult.email,
-                subAssignment: "00"
-                //$or: [{assignmentID: /.00$/}, {assignmentID: /.0$/}] //Search for assignments with whole numbers
-                  //shared: true
-            })
-//            .limit( 5 )   //Do we want to load every single whole number assignment, or just some? Query might be time intensive.
-            .sort({
-                assignmentID: -1
-            })
-            .exec(function(err, assignmentResult) {
-                if (err) return next(err)
+                        var assig = []
+                        //console.log(assignmentResult);
+                        for (i = 0; i < assignmentResult.length; i++) {
+                            //if(assignmentResult[i].data)
+                               // assig.push(assignmentResult[i].assignmentID)
+                            assig.push(assignmentResult[i].assignmentNumber)
 
-                if (!assignmentResult) return next("could not find " +
-                                                     "assignment " + req.params.userNameRes)
+                        }
 
-                var assig = []
-                //console.log(assignmentResult);
-                for (i = 0; i < assignmentResult.length; i++) {
-                    //if(assignmentResult[i].data)
-                       // assig.push(assignmentResult[i].assignmentID)
-                    assig.push(assignmentResult[i].assignmentNumber)
+        //                        console.log(assignmentResult, assignmentResult.length);
 
-                }
+        //                        if(assignmentResult[i].assignmentNumber != "")
+        //                        assig.push(assignmentResult[i].assignmentNumber)
+        //                    else
 
-//                        console.log(assignmentResult, assignmentResult.length);
+                        getAssignments(assig, [], function(assignmentsRes, testString) {
 
-//                        if(assignmentResult[i].assignmentNumber != "")
-//                        assig.push(assignmentResult[i].assignmentNumber)
-//                    else
-
-                getAssignments(assig, [], function(assignmentsRes) {
-
-                          return res.render('assignments/gallery_2', {
-                                            "title": "Assignment gallery",
-                                            "user":req.user,
-                                            "usernames": req.params.userNameRes,
-                                            "assignments":assignmentsRes
-                                            })
-                          })
+                                  return res.render('assignments/gallery_2', {
+                                                    "title": "Assignment gallery",
+                                                    "user":req.user,
+                                                    "usernames": req.params.userNameRes,
+                                                    "assignments":assignmentsRes,
+                                                    "test":testString
+                                                  })
+                        })
+                    })
               })
-        })
 }
